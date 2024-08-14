1ac4565b211ae6d65651386b7fdea7da980246c3
rhevo <eugenenarh1@gmail.com>
2024-08-14 14:21:52 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,3 +1,4 @@
+'''
 #!/usr/bin/python3
 """
 Script that queries subscribers on a given Reddit subreddit.
@@ -17,3 +18,43 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+        '''
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+import sys
+
+def get_subscribers(subreddit):
+    """
+    Return the total number of subscribers on a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+    
+    Returns:
+        int: The number of subscribers. Returns 0 if the subreddit does not exist or the request fails.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+    except requests.RequestException as e:
+        print(f"Error: {e}", file=sys.stderr)
+        return 0
+    
+    data = response.json()
+    return data['data']['subscribers']
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <subreddit_name>", file=sys.stderr)
+        sys.exit(1)
+    
+    subreddit = sys.argv[1]
+    subscribers = get_subscribers(subreddit)
+    print(f"The subreddit r/{subreddit} has {subscribers} subscribers.")
